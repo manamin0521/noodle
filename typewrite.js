e228771f5a4a768da0e845c3383fdf49d045be9a
matsubara.manami <matsubara.manami@slogan.jp>
2016-07-08 08:51:39 +0900

    まだ変わらず

--- typewrite.js
+++ typewrite.js
@@ -1,9 +1,10 @@
 var buttonCounter = 0; //カウンタ変数
 function onClickButton(){
     if(buttonCounter == 0){
-  document.getElementById("RewritingDiv").innerHTML =
-      "<p>最初の書き換え</p>";
-  buttonCounter++;
+      target = document.getElementById("RewritingDiv");
+      console.log(target)
+      target.innerHTML ="<p>最初の書き換え</p>";
+      buttonCounter++;
     }
     else if(buttonCounter == 1){
   document.getElementById("RewritingDiv").innerHTML =
@@ -16,3 +17,18 @@ function onClickButton(){
   buttonCounter = 0;
     }
 }
+
+// var imgFlag = 0;
+// function onClickButton(){
+//     if(imgFlag == 0){
+//   document.getElementById("RewritingDiv").innerHTML =
+//       "<img src='http://k.yimg.jp/images/top/sp2/cmn/logo-ns-131205.png' alt='Yahooのロゴ' />";
+//   imgFlag = 1;
+//     }
+//     else{ //imgFlagが0以外の時
+//   document.getElementById("RewritingDiv").innerHTML =
+//       "<img src='https://www.google.co.jp/images/srpr/logo11w.png' alt='Googleのロゴ' />";
+//   imgFlag = 0;
+//     }
+
+// }
