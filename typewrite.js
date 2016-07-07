86ffa683cefa8ace2269e6e3a269063a8ef3c473
matsubara.manami <matsubara.manami@slogan.jp>
2016-07-08 08:03:31 +0900

    文章変わった後タイプライター風に出ない

--- typewrite.js
+++ typewrite.js
@@ -1,19 +1,18 @@
-var buttonCounter = 0;
+var buttonCounter = 0; //カウンタ変数
 function onClickButton(){
-  console.log("OK")
-  
     if(buttonCounter == 0){
-  document.getElementById(".RewritingDiv").innerHTML =
-      "<p>最初の書き換え<a class="switch" onclick="onClickButton();"><u>▽</u></a></p>;"
+  document.getElementById("RewritingDiv").innerHTML =
+      "<p>最初の書き換え</p>";
   buttonCounter++;
     }
     else if(buttonCounter == 1){
-  document.getElementById(".RewritingDiv").innerHTML =
-      "<p>2回目の書き換え"<a class="switch" onclick="onClickButton();><u>▽</u></a></p>;"
+  document.getElementById("RewritingDiv").innerHTML =
+      "<p>2回目の書き換え</p>";
   buttonCounter++;
     }
     else if(buttonCounter == 2){
-  document.getElementById(".RewritingDiv").innerHTML =
-      "<p>3回目の書き換え．<br />最初に戻ります<a class="switch" onclick="onClickButton();"><u>▽</u></a></p>;"
+  document.getElementById("RewritingDiv").innerHTML =
+      "<p>3回目の書き換え．<br />最初に戻ります．</p>";
   buttonCounter = 0;
     }
+}
