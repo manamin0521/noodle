efa56d345bb7ebf6904d8749b614409d92cd0205
matsubara.manami <matsubara.manami@slogan.jp>
2016-07-08 11:48:38 +0900

    画面遷移後文字でない、タイプライター表示されない

--- typewrite.js
+++ typewrite.js
@@ -3,32 +3,68 @@ function onClickButton(){
     if(buttonCounter == 0){
       target = document.getElementById("RewritingDiv");
       console.log(target)
-      target.innerHTML ="<p>最初の書き換え</p>";
+      target.innerHTML =
+      "<center><img src='./ra1.png' alt='ラーメンくん' /></center>";
+      "<p>最初の書き換え</p>";
       buttonCounter++;
     }
     else if(buttonCounter == 1){
   document.getElementById("RewritingDiv").innerHTML =
+      "<center><img src='./ゆなみや店長.png'　width='150' height='500'></center>";
       "<p>2回目の書き換え</p>";
   buttonCounter++;
     }
     else if(buttonCounter == 2){
   document.getElementById("RewritingDiv").innerHTML =
-      "<p>3回目の書き換え．<br />最初に戻ります．</p>";
+      "<center><img src='./ra1.png' alt='ラーメンくん' /></center>";
+      "<p>2回目の書き換え</p>";
+  buttonCounter++;
+    }
+    else if(buttonCounter == 3){
+  document.getElementById("RewritingDiv").innerHTML =
+      "<center><img src='./ゆなみや店長.png'　width='150' height='500'></center>";
+      "<p>2回目の書き換え</p>";
+  buttonCounter++;
+    } 
+    else if(buttonCounter == 4){
+  document.getElementById("RewritingDiv").innerHTML =
+      "<center><img src='./ゆなみや店長.png'　width='150' height='500'></center>";
+      "<p>2回目の書き換え</p>";
+  buttonCounter++;
+    }
+    else if(buttonCounter == 5){
+  document.getElementById("RewritingDiv").innerHTML =
+      "<center><img src='./ra1.png' alt='ラーメンくん' /></center>";
+      "<p>2回目の書き換え</p>";
+  buttonCounter++;
+    }
+    else if(buttonCounter == 6){
+  document.getElementById("RewritingDiv").innerHTML =
+      "<center><img src='./ゆなみや店長.png'　width='150' height='500'></center>";
+      "<p>2回目の書き換え</p>";
+  buttonCounter++;
+    } else if(buttonCounter == 7){
+  document.getElementById("RewritingDiv").innerHTML =
+      "<center><img src='./ゆなみや店長.png'　width='150' height='500'></center>";
+      "<p>2回目の書き換え</p>";
+  buttonCounter++;
+    }
+    else if(buttonCounter == 8){
+  document.getElementById("RewritingDiv").innerHTML =
+      "<center><img src='./ra1.png' alt='ラーメンくん' /></center>";
+      "<p>2回目の書き換え</p>";
+  buttonCounter++;
+    }
+    else if(buttonCounter == 9){
+  document.getElementById("RewritingDiv").innerHTML =
+      "<center><img src='./ゆなみや店長.png'　width='150' height='500'></center>";
+      "<p>2回目の書き換え</p>";
+  buttonCounter++;
+    } 
+    else if(buttonCounter == 10){
+  document.getElementById("RewritingDiv").innerHTML =
+      "<center><img src='./ra1.png' alt='ラーメンくん' /></center>";
+      "<p>3回目の書き換え</p>";
   buttonCounter = 0;
     }
 }
-
-// var imgFlag = 0;
-// function onClickButton(){
-//     if(imgFlag == 0){
-//   document.getElementById("RewritingDiv").innerHTML =
-//       "<img src='http://k.yimg.jp/images/top/sp2/cmn/logo-ns-131205.png' alt='Yahooのロゴ' />";
-//   imgFlag = 1;
-//     }
-//     else{ //imgFlagが0以外の時
-//   document.getElementById("RewritingDiv").innerHTML =
-//       "<img src='https://www.google.co.jp/images/srpr/logo11w.png' alt='Googleのロゴ' />";
-//   imgFlag = 0;
-//     }
-
-// }
